53e2cf1921dc6e3d741f69f143fe4dd887f96206
Tony Izuogu <anthoizu@yahoo.com>
2023-12-20 20:31:45 -0600

    Updated the haggle logic

--- haggle/src/components/ProductImage/ProductImage.jsx
+++ haggle/src/components/ProductImage/ProductImage.jsx
@@ -1,7 +1,50 @@
 import "./ProductImg.css";
 import basket from "../assets/voucher.jpg";
+import { useState } from "react";
 
 function ProdImg() {
+  const [counterOff, setcounterOff] = useState(0);
+  const [offersubmit, setoffersubmit] = useState(0);
+
+  function offerChange() {
+    setoffersubmit(submitOffer);
+  }
+
+  const asking = 45;
+  let Offer = 42; // Use State??
+  const OfferTen = asking * 0.1;
+  const OfferFive = asking * 0.05;
+  const OfferTwenty = asking * 0.2;
+
+  let counterOffer = null;
+  // function prom() {
+  //   prompt("Please type in your offer");
+  // }
+
+  function changeOffer() {
+    setcounterOff(asking * 0.4);
+  }
+
+  function OfferPrice(e) {
+    // const Offer = 0;
+    // let Offer = e.trarget.value;
+    // const LAST_PRICE = askingPrice * 0.15;
+    // let Offered_price = e.target.value;
+
+    if (Offer >= asking - OfferTen) {
+      // let counterOffer = asking * 0.5;
+      alert(asking - OfferTen);
+      alert("Congratulations! Your Offer has been accepted");
+      // counterOffer = asking * 0.6;
+    } else {
+      alert("Your offer is not accepted");
+    }
+  }
+
+  function submitOffer(e) {
+    //  e.target.value;
+  }
+
   return (
     <>
       <div
@@ -21,16 +64,23 @@ function ProdImg() {
         />
         {/* <img src="..." class="card-img-top" alt="..."> */}
         <div className="card-body">
-          <h5 className="card-title">Voucher</h5>
+          <h5 className="card-title">Shopping Voucher</h5>
           <div>
-            <p>Asking Price: $100 </p>
-            <p>Current Offer: $ </p>
+            <p>Asking Price: ${asking} </p>
+            <p>Current Offer: $ {Offer}</p>
+            {/* <p>Counter Offer: $ {counterOffer}</p> */}
           </div>
           <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-            <button onClick={""} className="btn btn-primary">
+            <button onClick={OfferPrice} className="btn btn-primary">
               Make Offer
             </button>
-            <button className="btn btn-primary">Checkout</button>
+            <input
+              onKeyChange={submitOffer}
+              type="text"
+              placeholder="Type your amount"
+              style={{ width: "100px", height: "35px" }}
+            />
+            {/* <button className="btn btn-primary">Checkout</button> */}
           </div>
         </div>
       </div>
