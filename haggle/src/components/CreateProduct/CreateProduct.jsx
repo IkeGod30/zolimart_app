0b3b3fa98c81af646270833df40d6d59fae4b6cb
Tony Izuogu <anthoizu@yahoo.com>
2024-01-28 21:28:20 -0600

    Added test object

--- haggle/src/components/CreateProduct/CreateProduct.jsx
+++ haggle/src/components/CreateProduct/CreateProduct.jsx
@@ -1,21 +1,39 @@
 // Component for Creating Product Detail Inputs/parameters //
+import { useState } from "react";
 
 export default function NewProduct() {
-  const details = {
+  // const details = {
+  //   description: "Computer",
+  //   specs: "HP Laptop",
+  //   quiz: "Follow Me",
+  // };
+  const [specify, setSpecify] = useState({
     description: "Computer",
     specs: "HP Laptop",
     quiz: "Follow Me",
-    // pic: "../assets/voucher.jpg",
-  };
+  });
+  function changeSpecs() {
+    setSpecify({
+      ...specify,
+      specs: "Saints In Hell",
+      quiz: "Merchant of Venice",
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
     <div>
-      <form>
-        {/* <marquee>
-          <label>{details.quiz}</label>
-          <br />
-          <label>{details.description}</label>
-          <label>{details.pic}</label>
-        </marquee> */}
+      <form onSubmit={handleSubmit}>
+        <label>{specify.quiz}</label>
+        <br />
+        <label>{specify.description}</label>
+        <br />
+        <label>{specify.specs}</label>
+        <br />
+
+        <button onClick={changeSpecs}>Change Specs</button>
       </form>
     </div>
   );
